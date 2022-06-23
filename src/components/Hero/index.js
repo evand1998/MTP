e662037ab26c233c5b28ea035dd5a7a4cdb6679b
Evan Diaz <evand1998@gmail.com>
2022-06-23 13:43:59 -0500

    fixins

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import './style.css';
 
-// PULL IN THE NESSESSARY PROPS
-function Header({ currentPage, handlePageChange }) {
+function Hero() {
 
     return (
-        <nav>
-            <nav id="navbar" className="nav-menu navbar1">
-                {/* ON EACH CLICK, UPDATE THE 'CURRENT-PAGE' THROUGH THE 'HANDLE-PAGE-CHANGE' */}
-                {/* WHEN ACTIVE, ADD AN 'ACTIVE' CLASS FOR HIGHLIGHTING THE NAV SELECTION */}
-                <ul className="justify-content-center">
-                    <li>
-                        <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link scrollto' : 'nav-link scrollto'} ><span>About Me</span></a>
-                    </li>
-                    <li>
-                        <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Resume</span></a>
-                    </li>
-                    <li>
-                        <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Portfolio</span></a>
-                    </li>
-                    <li>
-                        <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Contact</span></a>
-                    </li>
-                </ul>
-            </nav>
-        </nav>
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+                <h1>Evan S. Diaz</h1>
+                <p>Full Stack Web Developer</p>
+            </div>
+        </section>
     )
 }
 
-export default Header;
+export default Hero;
