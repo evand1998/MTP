c38b784e7df202de901d375e191ad96d4319a7f6
Evan Diaz <evand1998@gmail.com>
2022-06-24 01:08:33 -0500

    about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,7 +22,7 @@ function About() {
                         Throughout my entire life, I've had a passion for building; manifesting something from the ground up, to the point of success.
                         </p>
                         <p>
-                        I am currently pursuing a degree in business management at the University of Houston, but wanted to expand my skillset to meet the demands of today’s market. This desire led me to enroll in a coding program offered by Rice University, during which I developed a broad range of differing applications. Through the combination of these skills and my education, I hope to make myself an asset in the workplace. If you are seeking a diligent and self-motivated addition to your team, look no further - let’s build something together.
+                        I am currently pursuing a degree in business management at the University of Houston, but wanted to expand my skillset to meet the demands of today’s market. This desire led me to enroll in a coding program offered by Rice University, during which I developed a broad range of differing applications. Through the combination of these skills and my education, I hope to make myself an asset in the workplace. If you are seeking a diligent and self-motivated addition to your team, look no further - let’s create something great.
                         </p>
                     </div>
                 </div>
